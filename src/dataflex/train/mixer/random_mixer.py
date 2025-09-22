fa19ec115d0a4e10937151d174745a0a59715c9b
TheRoadQaQ <maluqaq@163.com>
2025-09-22 13:16:16 +0800

    add mix_trainer_base_mixer

--- src/dataflex/train/mixer/random_mixer.py
+++ src/dataflex/train/mixer/random_mixer.py
@@ -1,24 +1,16 @@
-from typing import List
-import torch.distributed as dist
-from tqdm import tqdm
-from torch.utils.data import DataLoader
-import numpy as np
-import logging
-import sys
-logging.basicConfig(level=logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
 from dataflex.core.registry import register_mixer
 from dataflex.utils.logging import logger
+from .base_mixer import Mixer
+
+import numpy as np
 
 @register_mixer("random")
-class RandomMixer:
-    def __init__(self, seed, mixture_manager):
+class RandomMixer(Mixer):
+    def __init__(self, mixture_manager, seed):
+        super().__init__(mixture_manager)
         self.seed = seed
-        self.mixture_manager = mixture_manager
     
-    def mix(self) -> np.ndarray:
+    def mix(self, model, step_id: int, **kwargs) -> np.ndarray:
         """
         随机生成一组比例向量。
 
@@ -26,9 +18,10 @@ class RandomMixer:
             np.ndarray: 长度为源数量的归一化比例数组。
         """
         k = len(self.mixture_manager.names)
+        np.random.seed(self.seed)
         raw = np.random.random(k)
         probs = raw / raw.sum()  # 归一化
-        logger.info(f"[RandomMixer] Generated proportions: {probs}")
+        logger.info(f"[RandomMixer] Step {step_id} Generated proportions: {probs}")
 
         return probs
 
